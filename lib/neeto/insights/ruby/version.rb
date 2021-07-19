ae696ba9c66610e463e1937fa310481f09a34baa
Unnikrishnan KP <unni.tallman@gmail.com>
2021-07-20 01:56:02 +0530

    first commit

---
+++ lib/neeto/insights/ruby/version.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module Neeto
+  module Insights
+    module Ruby
+      VERSION = "0.1.0"
+    end
+  end
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+$LOAD_PATH.unshift File.expand_path("../lib", __dir__)
+require "neeto/insights/ruby"
+
+require "test-unit"
